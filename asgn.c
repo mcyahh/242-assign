4418fa5fa22190ff8087a1faacaa6f77fd7ff83d
mcyahh <31835241+mcyahh@users.noreply.github.com>
2017-09-15 15:11:06 +1200

    Update asgn.c

--- asgn.c
+++ asgn.c
@@ -52,6 +52,7 @@ int main(int argc, char **argv) {
     int unknown = 0; /* A count of the words not found in the hash table */
 
     if (argc == 1) { /* No options or files */
+        fprintf(stderr, "No valid option choosen, run with -h for help")
         return EXIT_FAILURE;
     }
 
